48d142882cef0839b8a4db32d75e6f436a19243f
AminAvanessian <amin.avanessianzadeh@uoit.net>
2018-10-03 00:06:40 -0400

    deposit money

--- EchoClient.java
+++ EchoClient.java
@@ -27,7 +27,7 @@ public class EchoClient {
             String userInput;
             while ((userInput = stdIn.readLine()) != null) {
                 out.println(userInput);
-                System.out.println("Echo Server Sent: " + in.readLine());
+                System.out.println("Bank Server Sent: " + in.readLine());
             }
         } catch (UnknownHostException e) {
             System.err.println("Don't know about host " + hostName);
--- MTEchoServer.java
+++ MTEchoServer.java
@@ -1,5 +1,8 @@
 import java.net.*;
+import java.util.HashMap;
 import java.io.*;
+import java.util.*;
+
  
 public class MTEchoServer {
 
@@ -23,11 +26,36 @@ public class MTEchoServer {
                Connection cc = new Connection(client);
             }
         } catch(Exception e) {
-           System.out.println("Exception: "+e);
+           System.out.println("Exception: " + e);
         }
     }
-         
-  
+}
+
+class BankServer {
+    // user ip and amount of $
+    public static Map<String,Integer> usersData = new HashMap<String,Integer>();  
+
+    // protocol commands
+    public static final String DEPOSIT_MONEY = "DEPOSIT";
+    public static final String TRANSFER_MONEY = "TRANSFER";
+    public static final String CHECK_BALANCE = "CHECK";
+
+    // transfer money from sender to receiver
+    public static void transferMoney(String sender, String receiver, Integer amount) {
+        // subtract money from sender
+
+        // add money to receiver
+
+    }
+
+    // deposit money 
+    public static void depositMoney(String client, int amount) {
+        // get users bank balance
+        int balance = BankServer.usersData.get(client);
+
+        // add money to users account
+        usersData.put(client, balance + amount);
+    }
 }
 
 class Connection extends Thread {
@@ -37,17 +65,15 @@ class Connection extends Thread {
 
     public Connection(Socket s) { // constructor
        client = s;
-       
 
        try {
            out = new PrintWriter(client.getOutputStream(), true);                   
-           in = new BufferedReader(
-                new InputStreamReader(client.getInputStream()));
+           in = new BufferedReader(new InputStreamReader(client.getInputStream()));
        } catch (IOException e) {
            try {
              client.close();
            } catch (IOException ex) {
-             System.out.println("Error while getting socket streams.."+ex);
+             System.out.println("Error while getting socket streams.." + ex);
            }
            return;
        }
@@ -56,11 +82,47 @@ class Connection extends Thread {
  
     public void run() {
       try {
-
          String inputLine;
+         String clientSocketAddress = client.getRemoteSocketAddress().toString();
+         String clientAddress = clientSocketAddress.substring(1);
+
+         // add user data to map
+         BankServer.usersData.put(clientAddress, 5000);
+
          while ((inputLine = in.readLine()) != null) {
-                System.out.println("Received from: "+ client.getRemoteSocketAddress() + " Input: "+inputLine);
-                out.println(inputLine);
+                System.out.println("Received from: " + clientAddress + " Input: " + inputLine);
+                String[] userCommands = inputLine.split(" ");
+                String firstCommand = userCommands[0];
+
+                String returnText = "Please choose a valid request: DEPOSIT | CHECK | TRANSFER";
+
+                switch(firstCommand) {
+                    case BankServer.DEPOSIT_MONEY:
+                        // get transfer ammount
+                        int amount = Integer.parseInt(userCommands[1]);
+
+                        // add money to users account 
+                        BankServer.depositMoney(clientAddress, amount);
+
+                        // set return text to user
+                        returnText = "$" + amount + " was deposited into your account";
+                        break;
+
+                    case BankServer.CHECK_BALANCE:
+                        System.out.println("YOU CHECK BALANCE!");
+                        
+                        break;
+                    
+                    case BankServer.TRANSFER_MONEY: 
+                        System.out.println("YOU TRANSFER MONEY");
+                    
+                        break;
+                }
+                out.println(returnText);
+
+            for (Map.Entry m:BankServer.usersData.entrySet()) {  
+                System.out.println(m.getKey()+" "+m.getValue());  
+            }  
          }
          client.close();
        } catch (IOException e) {
