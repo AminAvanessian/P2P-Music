9ea933aa192cc9dd041960962232ee338a58e091
AminAvanessian <amin.avanessianzadeh@uoit.net>
2018-11-22 20:09:42 -0500

    Added implementation details

--- UDPMulticastClient.java
+++ UDPMulticastClient.java
@@ -1,31 +1,41 @@
 import java.io.IOException;
 import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 
 public class UDPMulticastClient implements Runnable {
+   String ipAddress = "239.0.0.0";
 
    public static void main(String[] args) {
-      Thread t=new Thread(new UDPMulticastClient());
+      Thread t = new Thread(new UDPMulticastClient());
       t.start();
+      System.out.println("Search for song...");
    }
 
-   public void receiveUDPMessage(String ip, int port) throws
-         IOException {
-      byte[] buffer=new byte[1024];
-      MulticastSocket socket=new MulticastSocket(4321);
-      InetAddress group=InetAddress.getByName("230.0.0.0");
+   public void receiveUDPMessage(String ip, int port) throws IOException {
+      byte[] buffer = new byte[1024];
+      MulticastSocket socket = new MulticastSocket(4321);
+      InetAddress group = InetAddress.getByName(ipAddress);
       socket.joinGroup(group);
-      while(true){
+      
+      while (true) {
          System.out.println("Waiting for multicast message...");
-         DatagramPacket packet=new DatagramPacket(buffer,
-            buffer.length);
+
+         DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
          socket.receive(packet);
-         String msg=new String(packet.getData(),
-         packet.getOffset(),packet.getLength());
-         System.out.println("[Multicast UDP message received] >> "+msg);
-         if("OK".equals(msg)) {
-            System.out.println("No more message. Exiting : "+msg);
+         String msg = new String(packet.getData(), packet.getOffset(), packet.getLength());
+
+         // print the IP of the sender
+         String userAddress = packet.getSocketAddress().toString().substring(1);
+         String userIP = userAddress.substring(0, userAddress.indexOf(":"));
+
+         System.out.println("Packet sender address: " + userIP);
+
+         System.out.println("[Multicast UDP message received] >> " + msg);
+
+         if ("OK".equals(msg)) {
+            System.out.println("No more message. Exiting : " + msg);
             break;
          }
       }
@@ -36,8 +46,8 @@ public class UDPMulticastClient implements Runnable {
    @Override
    public void run() {
       try {
-         receiveUDPMessage("230.0.0.0", 4321);
-      } catch(IOException ex){
+         receiveUDPMessage(ipAddress, 4321);
+      } catch(IOException ex) {
          ex.printStackTrace();
       }
    }
--- UDPMulticastServer.java
+++ UDPMulticastServer.java
@@ -3,24 +3,47 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
+/* 
+    P2P Protocol
+    ------------
+
+    Client Methods
+    - requestSong(): request .mp3 file from clients to see who has the song
+    - confirmSong(): let requestor know that client has the song they are looking for
+    - receiveSong(): receive .mp3 file from client (sender)
+    - sendSong(): send .mp3 file to requestor  
+    - songNotFound(): let requestor know that client no longer has the .mp3 file
+    - getLocalSongs(): get local .mp3 files owned by the client on start-up
+    - receiveUDPMessage(): receive UDP message from other client(s)
+
+    Message Targets
+    - ALL: Message that is intended for all clients on the P2P network
+    - DIRECTED: Message that is intended for a specific client on the P2P Network
+
+    Message Types
+    - Request
+    - Confirm
+    - Send
+    - Receive
+    - NotFound
+*/
+
 public class UDPMulticastServer {
+    static String ipAddress = "239.0.0.0";
 
-   public static void sendUDPMessage(String message, String ipAddress, int port) throws IOException {
-      DatagramSocket socket = new DatagramSocket();
-      InetAddress group = InetAddress.getByName(ipAddress);
-      byte[] msg = message.getBytes();
-      DatagramPacket packet = new DatagramPacket(msg, msg.length, group, port);
-      socket.send(packet);
-      socket.close();
-   }
+    public static void sendUDPMessage(String message, String ipAddress, int port) throws IOException {
+        DatagramSocket socket = new DatagramSocket();
+        InetAddress group = InetAddress.getByName(ipAddress);
+        byte[] msg = message.getBytes();
+        DatagramPacket packet = new DatagramPacket(msg, msg.length, group, port);
 
-   public static void main(String[] args) throws IOException {
-      sendUDPMessage("This is a multicast messge", "230.0.0.0",
-         4321);
-      sendUDPMessage("This is the second multicast messge",
-         "230.0.0.0", 4321);
-      sendUDPMessage("This is the third multicast messge",
-         "230.0.0.0", 4321);
-      sendUDPMessage("OK", "230.0.0.0", 4321);
-   }
+        socket.send(packet);
+        socket.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        sendUDPMessage("This is a multicast messge", ipAddress, 4321);
+        sendUDPMessage("This is the second multicast messge", ipAddress, 4321);
+        sendUDPMessage("OKKK", ipAddress, 4321);
+    }
 }
